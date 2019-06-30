2df7e0b8d9a74db6aee6a5bd89819046d672c2c6
Shaurya Dhadwal <shauryadhadwal@gmail.com>
2019-06-30 17:19:32 +0530

    gulp file edited

--- gulpfile.js
+++ gulpfile.js
@@ -3,35 +3,89 @@ const { parallel } = require('gulp');
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
 const uglify = require('gulp-uglify');
-//Compile scss to css
+const inject = require('gulp-inject');
+const htmlclean = require('gulp-htmlclean');
+const cleanCSS = require('gulp-clean-css');
+const concat = require('gulp-concat');
 
-function style() {
-    return gulp.src('./styles/**/*.scss')
+const paths = {
+    src: 'src/**/*',
+    srcHTML: 'src/**/*.html',
+    srcSASS: 'src/**/*.scss',
+    srcJS: 'src/**/*.js',
+    tmp: 'tmp',
+    tmpIndex: 'tmp/index.html',
+    tmpCSS: 'tmp/**/*.css',
+    tmpJS: 'tmp/**/*.js',
+    dist: 'dist',
+    distIndex: 'dist/index.html',
+    distCSS: 'dist/**/*.css',
+    distJS: 'dist/**/*.js'
+};
+
+gulp.task('html', function () {
+    return gulp.src(paths.srcHTML).pipe(gulp.dest(paths.tmp));
+});
+
+gulp.task('css', function () {
+    return gulp.src(paths.srcSASS).pipe(sass()).pipe(gulp.dest(paths.tmp));
+});
+
+gulp.task('js', function () {
+    return gulp.src(paths.srcJS).pipe(gulp.dest(paths.tmp));
+});
+
+gulp.task('copy', gulp.parallel('html', 'css', 'js'));
+
+gulp.task('inject', gulp.series('copy', function () {
+    var css = gulp.src(paths.tmpCSS);
+    var js = gulp.src(paths.tmpJS);
+    return gulp.src(paths.tmpIndex)
+        .pipe(inject(css, { relative: true }))
+        .pipe(inject(js, { relative: true }))
+        .pipe(gulp.dest(paths.tmp));
+}));
+
+// gulp.task('watch', ['inject'], function(){
+//     browserSync.init({
+//         server: {
+//             baseDir: './' + paths.tmp,
+//         },
+//         port: 3000
+//     });
+//     gulp.watch(paths.src).on('change', browserSync.reload);
+// });
+
+gulp.task('html:dist', function () {
+    return gulp.src(paths.srcHTML)
+        // .pipe(htmlclean())
+        .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('css:dist', function () {
+    return gulp.src(paths.srcSASS)
         .pipe(sass())
-        .pipe(gulp.dest('./public/styles'))
-        .pipe(browserSync.stream());
-}
-function script() {
-    return gulp.src('./scripts/**/*.js')
+        .pipe(concat('style.min.css'))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(paths.dist));
+});
+
+gulp.task('js:dist', function () {
+    return gulp.src(paths.srcJS)
+        .pipe(concat('script.min.js'))
         .pipe(uglify())
-        .pipe(gulp.dest('./public/scripts'))
-        .pipe(browserSync.stream());
-}
+        .pipe(gulp.dest(paths.dist));
+});
 
-function watch() {
-    browserSync.init({
-        server: {
-            baseDir: './',
-        },
-        port: 3000
-    });
-    gulp.watch('./styles/**/*.scss', style);
-    gulp.watch('./scripts/**/*.js', script);
-    gulp.watch('./*.html').on('change', browserSync.reload);
-    gulp.watch('./scripts/**/*.js').on('change', browserSync.reload);
-}
+gulp.task('copy:dist', gulp.parallel('html:dist', 'css:dist', 'js:dist'));
 
-exports.build = parallel(style, script)
-exports.script = script;
-exports.style = style;
-exports.watch = watch;
+gulp.task('inject:dist', gulp.series('copy:dist', function () {
+    var css = gulp.src(paths.distCSS);
+    var js = gulp.src(paths.distJS);
+    return gulp.src(paths.distIndex)
+        .pipe(inject(css, { relative: true }))
+        .pipe(inject(js, { relative: true }))
+        .pipe(gulp.dest(paths.dist));
+}));
+
+gulp.task('build', gulp.series('inject:dist'));
